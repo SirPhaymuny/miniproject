c22df3719e63885900ebb155bc1b270325436cad
SirPhaymuny <phaywork2@gmail.com>
2024-08-16 21:32:12 +0700

    preview images

--- cart.js
+++ cart.js
@@ -4,6 +4,9 @@ let minusBtn = document.querySelector(".minusBtn");
 let backBtn = document.querySelector(".backBtn"); 
 let card = document.querySelector("#card1"); 
 let preImg = document.querySelector("#pre-image"); 
+let preImg1 = document.querySelector("#pre-image1"); 
+let preImg2 = document.querySelector("#pre-image2"); 
+let preImg3 = document.querySelector("#pre-image3"); 
 let preImg4 = document.querySelector("#pre-image4"); 
 if (qty.innerHTML < 1) {
     qty.innerHTML = 1;
@@ -24,8 +27,18 @@ minusBtn.addEventListener("click", () => {
     }
 })
 
+//check on privew to change image
 backBtn.addEventListener("click", ()=> {
     window.location.href = "/index.html"
+})
+preImg1.addEventListener("click", () =>{
+    preImg.src = preImg1.src; 
+})
+preImg2.addEventListener("click", () =>{
+    preImg.src = preImg2.src; 
+})
+preImg3.addEventListener("click", () =>{
+    preImg.src = preImg3.src; 
 })
 preImg4.addEventListener("click", () =>{
     preImg.src = preImg4.src; 
